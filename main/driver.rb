2594bf80dedb3ec05c4f4c71c1e190160d6033e3
Ahmed <ahmed_alamer@aol.com>
2015-10-17 21:29:53 -0400

    refactored the driver

--- main/driver.rb
+++ main/driver.rb
@@ -1,23 +1,29 @@
 require '../main/pom'
 
-def main
-  file_handler = DataFileHandler.new('/home/ahmed/solar-coin-data/')
-
-  data_processor = DataProcessor.new
-
+def aggregate_claimants_data(data_processor, file_handler)
   json_data = file_handler.load_data_from_files
-
-  Logger.debug('Processing')
-
   data = data_processor.read_data(json_data)
-
   file_handler.write_json_file(data, 'result')
 
-  result = file_handler.read_json_file('result')
+  file_handler.read_json_file('result')
+end
 
+def generate_sql_file(data_processor, file_handler, result)
   sql_statements = data_processor.generate_sql_statements(result)
 
   file_handler.write_sql_statements(sql_statements, 'result')
+end
+
+DATA_DIRECTORY = '/home/ahmed/solar-coin-data/'
+
+def main
+  file_handler = DataFileHandler.new(DATA_DIRECTORY)
+  data_processor = DataProcessor.new
+
+  Logger.debug('Processing')
+
+  data = aggregate_claimants_data(data_processor, file_handler)
+  generate_sql_file(data_processor, file_handler, data)
 
   Logger.debug('Complete!')
 end
